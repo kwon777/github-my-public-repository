8fa5755407bb8db73733b8d01a3a3b3cb1159f2d
daekwon <dae.kwon@navy.mil>
2018-02-09 11:46:03 -0800

    Create Project

---
+++ my-first-project-forGit/src/MyClass.java
@@ -0,0 +1,29 @@
+
+/**
+ * A test class
+ * 
+ * <p>
+ * Date Created: Feb 9, 2018
+ * <p>
+ * $LastChangedBy$
+ * <br>
+ * $LastChangedDate$
+ * <br>
+ * $LastChangedRevision$
+ *
+ * @author Dan
+ * @version 1.0
+ */
+public class MyClass
+{
+
+   /**
+    * Main Method
+    * @param args
+    */
+   public static void main(String[] args)
+   {
+
+   }
+
+}
